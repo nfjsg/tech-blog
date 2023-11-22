1e4f8fab40dfb7624ff6431490d35bbec5f4d6dc
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-11-22 16:29:31 -0500

    Update server.js
    In this example:
    
    Express is used as the server framework.
    Sessions are managed using express-session and stored in a Sequelize store using connect-session-sequelize.
    Passport is set up for user authentication.
    Handlebars is used as the templating engine.
    Routes are imported from the controllers directory (you'll need to have your controllers defined there).

--- server.js
+++ server.js
@@ -1,28 +1,48 @@
 
 const express = require('express');
-const exphbs = require('express-handlebars');
 const session = require('express-session');
 const passport = require('passport');
 const path = require('path');
-const sequelize = require('./config/connection'); // Ensure to update the path based on your actual folder structure
-const routes = require('./controllers');
+const handlebars = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+// Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+// Session store
+const sequelize = require('./models').sequelize;
+const sessionStore = new SequelizeStore({
+  db: sequelize,
+});
 
-app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
+// Session setup
+app.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+  })
+);
+
+// Passport setup
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Handlebars setup
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Routes
+const routes = require('./controllers');
 app.use(routes);
 
+// Sync Sequelize models and start the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
