afb4fffa4ebd5706dba116e69bd48066e1752be7
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-11-22 16:02:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
 
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const passport = require('passport');
+const path = require('path');
+const sequelize = require('./config/connection'); // Ensure to update the path based on your actual folder structure
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+});
