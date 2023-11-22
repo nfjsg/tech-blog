0a210e1cf1e540e3fe5e56649465b3bf4bb85e7b
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-11-22 15:55:26 -0500

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,24 @@
 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
