fb20c5d125eb607159fcf9d987342168c74a1e05
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-11-22 15:55:45 -0500

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,20 @@
 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
