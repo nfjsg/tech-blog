c2ba03fe8647657a6015416c171bb3d24bd5af42
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-11-22 15:56:07 -0500

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,1 +1,28 @@
 
+const express = require('express');
+const router = express.Router();
+const { Post, User, Comment } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    res.render('home', { posts });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
